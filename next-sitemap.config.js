bab4317fecae16a8c1a8e32ae90092f5a6ac07ee
nimp <nimpweb@yandex.ru>
2022-06-21 10:30:38 +0500

    202206211030

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,41 @@
 const config = {
   siteUrl: process.env.siteUrl || 'https://webstudio56.ru',
+  changefreq: 'daily',
+  priority: 0.7,
+  sizemapSize: 5000,
   generateRobotTxt: true,
+  alternateRefs: [
+    {
+      href: 'http://webstudio56.ru',
+      hreflang: 'ru',
+    },
+    {
+      href: 'http://www.webstudio56.ru',
+      hreflag: 'ru',
+    },
+  ],
+  transform: async (confing, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastMod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+  robotTxtOptions: {
+    polices: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'http://webstudio56.ru/sitemap-0.xml',
+      'http://webstudio56.ru/sitemap-1.xml',
+      'http://webstudio56.ru/sitemap-2.xml',
+    ],
+  },
 };
 
 module.exports = config;
